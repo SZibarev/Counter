5ad7be294607c4d181fcf295d4a55c96474f5e7f
ZANZIBAR <serega.zibarev@mail.ru>
2025-08-01 22:17:40 -0700

    Initial Commit

---
+++ testTests/testTests.swift
@@ -0,0 +1,17 @@
+//
+//  testTests.swift
+//  testTests
+//
+//  Created by Сергей on 01.08.2025.
+//
+
+import Testing
+@testable import test
+
+struct testTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
